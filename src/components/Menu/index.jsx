b4e1a4ffcfa3512329a369b945f5a962dbf8d46f
erynrizky <eryn_r_napitupulu_x@telkomsel.co.id>
2023-07-07 15:30:27 +0700

    change qr code url to env

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -23,7 +23,7 @@ const Menu = ({ info }) => {
         <h1 className={styles.title}>Menu</h1>
         <p className={styles.content}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam ratione aut eos a explicabo et ducimus recusandae itaque rerum ut? Nihil ducimus illo maxime impedit fuga? Ab provident eaque fuga.</p>
         <div className={cn(styles.menu_wrapper,"flex_center")}>
-          <Button name={"Open Menu"} path={"https://cdn.qr-code-generator.com/account26986109/38121237_1.pdf?Expires=1688447552&Signature=XlaxLFDq9MEy5SxAeCegICw18DG0EOuatT20n0OxGZvIqHI5zHJUT8kvTakCMWOKuf3lceJHjDpRrMgmBxLkBDBrAaPbpCXWKuPLfse16szdUsIC7JA1U7uGZSdIh6HAzkYesbkDCgM72h9RiPFtiCwWuxcU3yfAw9dNRIuGa8vjlmC-oCM6M3FnxTUc5QtKLFS7MNhEzgdhi7ZuZKyKqbk62r4dUKdfnUL1tQhOYHYgMa~KGLRMFV7-pHZosBlLgXcE44jyjIBvUHMYuLtORuPk9rdsFBpTR4bWjAupOlhUgdlfeFZVkd76coswkiISp51Z5kOiOc-C2-UawjXg8g__&Key-Pair-Id=KKMPOJU8AYATR"}/>
+          <Button name={"Open Menu"} path={process.env.QR_CODE}/>
 
         </div>
       </div>
